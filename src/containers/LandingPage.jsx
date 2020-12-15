9426818711f3d173af0c054904d822d3f8ea68e9
Julian-Carelli <julianignaciocarelli@gmail.com>
2020-12-15 00:09:11 -0300

    first commmit

---
+++ src/containers/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import StyledLandingPage from './index';
+
+class LadingPage extends Component {
+
+    render(){
+        return(
+            <StyledLandingPage>
+                <h1 className="title">hola</h1>
+            </StyledLandingPage>
+        )
+    }
+}
+
+export default LadingPage
---
+++ src/containers/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const StyledLandingPage = styled.div `
+
+    .title {
+        font-family: 'Lato', sans-serif;
+        font-weight: 800;
+    }
+
+
+`;
+
+export default StyledLandingPage;
